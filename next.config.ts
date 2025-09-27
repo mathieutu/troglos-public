50f7cdb19ef512bba7121b1c4f251945578b2f5e
Mathieu TUDISCO <mathieu@tudisco.fr>
2025-09-27 11:20:20 +0200

    🔍️ Add redirects for old urls.

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,25 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  async redirects() {
+    return [
+      { permanent: true, source: '/decouvrir-la-speleo', destination: '/nous-rejoindre' },
+      { permanent: true, source: '/decouvrir-le-canyoning', destination: '/nous-rejoindre' },
+      { permanent: true, source: '/entrainements', destination: '/nous-rejoindre' },
+      { permanent: true, source: '/les-entrainements-au-mur', destination: '/nous-rejoindre' },
+      { permanent: true, source: '/equipement-recommande', destination: '/nous-rejoindre' },
+      { permanent: true, source: '/la-grotte-du-crochet-superieur', destination: '/' },
+      { permanent: true, source: '/les-sorties', destination: 'sorties' },
+      { permanent: true, source: '/les-classiques', destination: '/speleologie' },
+      { permanent: true, source: '/lexploration', destination: '/speleologie' },
+      { permanent: true, source: '/commission-scientifique', destination: '/' },
+      { permanent: true, source: '/partenariats', destination: '/club' },
+      { permanent: true, source: '/lequipe', destination: '/club' },
+      { permanent: true, source: '/lhistoire-du-club', destination: '/club' },
+      { permanent: true, source: '/comment-nous-rencontrer', destination: '/nous-rejoindre' },
+      { permanent: true, source: '/la-photo-souterraine', destination: '/' },
+    ]
+  },
 }
 
 export default nextConfig
