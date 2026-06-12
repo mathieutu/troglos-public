34d3b6af2e4837ade74862a3a3f7bbb9317abbed
Mathieu TUDISCO <mathieu@tudisco.fr>
2026-06-12 14:57:44 +0200

    📈 Add analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { SectionContainer } from '@/components/SectionContainer'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import { defaultMetadata } from '@/config/metadata'
+import Script from 'next/script'
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -19,6 +20,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+
+  const analyticsEnabled = process.env.NODE_ENV === 'production'
+    && (!process.env.NEXT_PUBLIC_VERCEL_ENV || process.env.NEXT_PUBLIC_VERCEL_ENV === 'production')
+
   return (
     <html lang="fr" className={`${spaceGrotesk.variable} scroll-smooth`}>
       <body className="dark overflow-x-hidden text-white antialiased">
@@ -27,6 +32,19 @@ export default function RootLayout({
           <main className="mb-auto">{children}</main>
           <Footer />
         </SectionContainer>
+        {/* Privacy-friendly analytics by Plausible */}
+        {analyticsEnabled && <Script async src="https://e.mathieutu.dev/js/pa-wE-0rabBwZxex4QEOLRX5.js" />}
+        {analyticsEnabled && (
+          <Script
+            id="next-plausible-init"
+            dangerouslySetInnerHTML={{
+              __html: `
+          window.plausible=window.plausible||function(){(plausible.q = plausible.q || []).push(arguments)},plausible.init=plausible.init||function(i){plausible.o = i || {}};
+          plausible.init()
+          `,
+            }}
+          />
+        )}
       </body>
     </html>
   )
