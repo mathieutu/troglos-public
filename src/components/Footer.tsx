f02a90b95875da4dccf9782c1e44eeb0779fed5d
Mathieu TUDISCO <mathieu@tudisco.fr>
2026-09-01 14:03:28 +0200

    ✨ Add Google Maps social link and change the address to cartes.app.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,6 +18,23 @@ export const Github = (svgProps: SVGProps<SVGSVGElement>) => (
   </svg>
 )
 
+export const GoogleMaps = (svgProps: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...svgProps}>
+    <title>Google Maps</title>
+    <defs>
+      <mask id="googleMapsMask">
+        <rect width="24" height="24" fill="white" />
+        <path
+          fill="black"
+          transform="translate(0.7 3) scale(0.95)"
+          d="M19.527 4.799c1.212 2.608.937 5.678-.405 8.173-1.101 2.047-2.744 3.74-4.098 5.614-.619.858-1.244 1.75-1.669 2.727-.141.325-.263.658-.383.992-.121.333-.224.673-.34 1.008-.109.314-.236.684-.627.687h-.007c-.466-.001-.579-.53-.695-.887-.284-.874-.581-1.713-1.019-2.525-.51-.944-1.145-1.817-1.79-2.671L19.527 4.799zM8.545 7.705l-3.959 4.707c.724 1.54 1.821 2.863 2.871 4.18.247.31.494.622.737.936l4.984-5.925-.029.01c-1.741.601-3.691-.291-4.392-1.987a3.377 3.377 0 0 1-.209-.716c-.063-.437-.077-.761-.004-1.198l.001-.007zM5.492 3.149l-.003.004c-1.947 2.466-2.281 5.88-1.117 8.77l4.785-5.689-.058-.05-3.607-3.035zM14.661.436l-3.838 4.563a.295.295 0 0 1 .027-.01c1.6-.551 3.403.15 4.22 1.626.176.319.323.683.377 1.045.068.446.085.773.012 1.22l-.003.016 3.836-4.561A8.382 8.382 0 0 0 14.67.439l-.009-.003zM9.466 5.868L14.162.285l-.047-.012A8.31 8.31 0 0 0 11.986 0a8.439 8.439 0 0 0-6.169 2.766l-.016.018 3.665 3.084z"
+        />
+      </mask>
+    </defs>
+    <circle cx="12" cy="12" r="12" fill="currentColor" mask="url(#googleMapsMask)" />
+  </svg>
+)
+
 export const Contact = (svgProps: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...svgProps}>
     <title>Contact</title>
@@ -38,6 +55,7 @@ const components = {
   contact: Contact,
   github: Github,
   facebook: Facebook,
+  googleMaps: GoogleMaps,
 }
 
 type SocialIconProps = {
@@ -66,6 +84,7 @@ export function Footer() {
           <SocialIcon kind="contact" href="/contact" size={6} />
           <SocialIcon kind="github" href="https://github.com/mathieutu/troglos-public" size={6} />
           <SocialIcon kind="facebook" href="https://www.facebook.com/troglos.fr" size={6} />
+          <SocialIcon kind="googleMaps" href="https://maps.app.goo.gl/ezzbEnmuuSUmBYtr6" size={6} />
         </div>
         <div className="text-sm text-gray-200">
           <Link className="hover:underline" href="https://app.troglos.fr">
@@ -77,7 +96,7 @@ export function Footer() {
             Clan Spéléo des Troglodytes
           </Link>
           <span>{` • `}</span>
-          <Link href="https://maps.app.goo.gl/ezzbEnmuuSUmBYtr6" className="hover:underline">
+          <Link href="https://cartes.app/?allez=Clan+Sp%25C3%25A9l%25C3%25A9ologique+des+Troglodytes%7Cn13851387775%7C4.88334%7C45.74014" className="hover:underline">
             18 rue Volney, 69008 Lyon
           </Link>
         </div>
